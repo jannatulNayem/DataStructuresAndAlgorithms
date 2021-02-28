f1e0b304ed2454f2a80075fffc7e877414f269d8
jannatulNayem <whoisjannatulnayem@gmail.com>
2021-02-28 14:29:55 +0600

    sparse array

--- Medium/sprase_array.js
+++ Medium/sprase_array.js
@@ -1,20 +1,71 @@
-function matchingStrings(string_arr, query_string){
-    let string_object = {}
+"use strict";
 
-    string_arr.forEach(element => {
-        if(element in string_object) string_object[element]++;
-        else string_object[element] = 1
-    });
+const fs = require("fs");
 
-    let output_arr = [];
-    query_string.forEach(element => {
-        if(element in string_object) output_arr.push(string_object[element])
-        else output_arr.push(0);
-    })
-    return output_arr
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .replace(/\s*$/, "")
+    .split("\n")
+    .map((str) => str.replace(/\s*$/, ""));
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
 }
 
-// sprase(["ab", "ab", "abc"], ["ab", "abc", "bc"])
-// sprase(["aba", "baba", "aba", "xzxb"], ["aba", "xzxb","ab"])
-// sprase(["def", "de", "fgh"], ["de", "Imn", "fgh"])
-sprase(["abcde", "sdaklfj", "asdjf", "na", "hasdn", "sdaklfj", "asdjf", "na", "asdjf", "na", "basdn", "sdaklfj", "asdjf"], ["abcde", "sdaklfj", "asdjf", "na", "basdn"] )
+// Complete the matchingStrings function below.
+function matchingStrings(string_arr, query_string) {
+  let string_object = {};
+
+  string_arr.forEach((element) => {
+    if (element in string_object) string_object[element]++;
+    else string_object[element] = 1;
+  });
+
+  let output_arr = [];
+  query_string.forEach((element) => {
+    if (element in string_object) output_arr.push(string_object[element]);
+    else output_arr.push(0);
+  });
+  return output_arr;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const stringsCount = parseInt(readLine(), 10);
+
+  let strings = [];
+
+  for (let i = 0; i < stringsCount; i++) {
+    const stringsItem = readLine();
+    strings.push(stringsItem);
+  }
+
+  const queriesCount = parseInt(readLine(), 10);
+
+  let queries = [];
+
+  for (let i = 0; i < queriesCount; i++) {
+    const queriesItem = readLine();
+    queries.push(queriesItem);
+  }
+
+  let res = matchingStrings(strings, queries);
+
+  ws.write(res.join("\n") + "\n");
+
+  ws.end();
+}
